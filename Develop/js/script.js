4afb8156db43c65b82a756da388daf7f4cee6086
gannacon <cwgannaway@gmail.com>
2021-04-17 17:25:18 -0500

    clean up code and add to README

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -52,7 +52,8 @@ function init() {
 
 }
 
-
+//------------------------------------------------------------------------------------------------------------
+// CHANGE COLOR OF BLOCK IS PAST PRESENT OR FUTURE USING COLORS NOT CLASSES
 
 const rows = document.getElementsByClassName("row");
 let currentHour = parseInt(moment().format('H'));
@@ -82,34 +83,5 @@ function setColor(element, color) {
   element.style.backgroundColor = color;
 }
 
-//------------------------------------------------------------------------------------------------------------
-// CHANGE COLOR OF BLOCK IS PAST PRESENT OR FUTURE
-
 
 init();
-
-//------------------------------------------------------------------------------------------------------------
-// COPIED CODE FROM ACTIVITIES FOR REF
-
-
-// // 2. What is the day of the week today?
-// var dayWeek = today.format('[Today is] dddd');
-// $('#2a').text(dayWeek);
-
-// // 3. Parse the following date, 11/3/2020, and convert it into the following format: Sunday, February 14th 2010, 3:25:50 pm.
-// var reformatDate = dayjs('11/3/20', 'MM-DD-YY').format(
-//   'dddd, MMMM Do YYYY, h:mm:ss a'
-// );
-// $('#3a').text(reformatDate);
-
-// // 4. I need to place my recycling bin on the curb on every odd week of the year for collection. Do I need to put out my recycling bin out this week?
-// var weekNum = today.week();
-// var takeOut;
-// // Check odd, then assign boolean
-// if (weekNum % 2) {
-//   takeOut = true;
-// } else {
-//   takeOut = false;
-// }
-
-// $('#4a').text(`${takeOut}, because it's currently week ${weekNum}`);
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Homework5-work_schedule
-Work Schedule
+# Work Scheduler
+The following is a work day scheduler build in javascript. This app uses bootstrap, jQuery, and Moment API's. Here is a link to my deployed page: https://gannacon.github.io/Homework5-work_schedule/
+
+## My Task
+
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+
+## User Story
+
+```md
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
